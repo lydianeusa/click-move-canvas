4b1d21716d20832e258e12c08a348b218ac3e00c
Lydiane Doner <lydiane.california@gmail.com>
2023-02-27 16:37:33 +0100

    boucle for

--- js/index.js
+++ js/index.js
@@ -1,19 +1,29 @@
 console.log("On est bien dans lotre fichier");
 window.addEventListener('load', init)
 let stage;
+let circles = [];
+
 function init() {
     let canvas = document.querySelector ('.myCanvas');
     stage = new createjs.Stage(canvas);
+   
+    let c1, c2, c3;
 
-    createCircle(200, 150, 30, "red")
-    createCircle(100, 150, 30, "green")
-    createCircle(200, 300, 100, "blue")
+    c1 = createCircle(200, 150, 30, "red")
+    c2 = createCircle(100, 150, 30, "green")
+    c3 = createCircle(200, 300, 100, "blue")
 
     createjs.Ticker.timingMode = createjs.Ticker.RAF;
     createjs.Ticker.addEventListener("tick", () => {
+        animate();
         stage.update()
     })
+    }
 
+    function animate () {
+      for(let index = 0; index < circles.length; index +=1) {
+        circles[index].y += 1
+      }
     }
 
 function createCircle (myX, myY, myRadius, myColor) {
@@ -27,6 +37,8 @@ stage.addChild(circle);
 circle.addEventListener('click', (event) => {
     console.log(event.currentTarget.myName);
 })
+
+circles.push(circle)
 
 return circle;
 }
